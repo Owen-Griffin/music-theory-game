8157e3d23fb89a793a4e41b8bca624de54389db2
Owen-Griffin <99355709+Owen-Griffin@users.noreply.github.com>
2024-08-03 22:10:31 -0400

    minor updates

--- addons/table_container/plugin.gd
+++ addons/table_container/plugin.gd
@@ -4,9 +4,9 @@ extends EditorPlugin
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
-	add_custom_type("Table", "PanelContainer", preload("Table.gd"), preload("icon.svg"))
+	pass
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
-	remove_custom_type("Table")
+	pass
--- scripts/ContactPopup.gd
+++ scripts/ContactPopup.gd
@@ -11,7 +11,6 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func _on_contact_button_pressed():
 	visibility = !visibility
 	
